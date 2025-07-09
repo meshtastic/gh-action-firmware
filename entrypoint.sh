cae0f11d0cd2af0182b4799cda765d308766536a
vidplace7 <vidplace7@gmail.com>
2025-07-08 20:25:45 -0400

    Gather ESP32 OTA bins AFTER build

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,13 +14,13 @@ MT_OTA_FIRMWARE_TARGET=${MT_OTA_FIRMWARE_TARGET:-""}
 
 # Build
 if [ "$MT_TARGET" = "build" ]; then
+    echo "Building PlatformIO environment: $MT_ENV"
+    /workspace/bin/build-"${MT_PLATFORM}".sh "$MT_ENV"
+    echo "Build artifacts are located at: $PLATFORMIO_BUILD_DIR"
     if [ -n "$MT_OTA_FIRMWARE_SOURCE" ] && [ -n "$MT_OTA_FIRMWARE_TARGET" ]; then
         echo "Downloading OTA firmware $MT_OTA_FIRMWARE_SOURCE from https://github.com/meshtastic/firmware-ota"
         curl -L "https://github.com/meshtastic/firmware-ota/releases/download/latest/$MT_OTA_FIRMWARE_SOURCE" -o "$MT_OTA_FIRMWARE_TARGET"
     fi
-    echo "Building PlatformIO environment: $MT_ENV"
-    /workspace/bin/build-"${MT_PLATFORM}".sh "$MT_ENV"
-    echo "Build artifacts are located at: $PLATFORMIO_BUILD_DIR"
 # Check
 elif [ "$MT_TARGET" = "check" ]; then
     /workspace/bin/check-all.sh "$MT_ENV"
